1ae3cbf08f70c5de5f4ec434dc6a09ef4ccaafd0
Juan Carlos <jcarloscm@ieee.org>
2022-04-16 13:05:17 -0500

    se inicia de nuevo el proceso de recepción x4

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def json_mensaje(mensaje):
 def reply_whatsapp(request: Request):
     response = MessagingResponse()
     response.message("Thanks for the image. Here's one for you!")
-    #return 'bandera'
+    return str(response)
 '''
     try:
         num_media = int(request.body.NumMedia)
